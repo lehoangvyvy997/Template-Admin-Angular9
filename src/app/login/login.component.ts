01495918ef6097742935842a9a0ba4c727ee6a01
Vỹ <15521036@gm.uit.edu.vn>
2020-03-01 21:33:26 +0700

    commit02

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -21,5 +21,7 @@ export class LoginComponent implements OnInit {
         console.log('test03');
         console.log('test01');
         console.log('test01');
+        console.log('test02');
+        console.log('test02');
     }
 }
