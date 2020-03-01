efe2f08f2b87a199e53b148637859dda4b01bde0
Vỹ <15521036@gm.uit.edu.vn>
2020-03-01 21:32:33 +0700

    commit01

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -18,7 +18,8 @@ export class LoginComponent implements OnInit {
     onLoggedin() {
         localStorage.setItem('isLoggedin', 'true');
         console.log('sssssssssssssssssssssssss');
-        console.log('test02');
         console.log('test03');
+        console.log('test01');
+        console.log('test01');
     }
 }
